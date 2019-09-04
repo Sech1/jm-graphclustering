19e0fa225d19b5d541b061538812151b1ed3d970
Jared M Schooley <jaschoo@siue.edu>
2019-09-04 17:23:10 -0500

    broke out clustering type functions and added parameter parsing for VAT, INT, TEN.

--- debugNetData/Program.cs
+++ debugNetData/Program.cs
@@ -14,16 +14,25 @@ using NetMining.Evaluation;
 using NetMining.ExtensionMethods;
 using System.IO;
 using System.Diagnostics;
+
 namespace debugNetData
 {
     class Program
     {
+        public enum OutType
+        {
+            TEN,
+            VAT,
+            INT
+        }
+
         static void Main(string[] args)
         {
             String workingDir = Directory.GetCurrentDirectory();
             if (args.Length == 0)
             {
-                System.Console.WriteLine("Usage: Program.cs <HealthyPath> <InfectedPath> <HealthyFile> <InfectedFile> ");
+                System.Console.WriteLine(
+                    "Usage: Program.cs <HealthyPath> <InfectedPath> <HealthyFile> <InfectedFile> ");
                 Environment.Exit(0);
             }
 
@@ -39,9 +48,9 @@ namespace debugNetData
             String infectedPath = $"{workingDir}//Data//{args[1]}";
             //"C:\\Users\\jmatta\\Dropbox\\Tayo\\Yasser\\graphs_John_Tayo\\CoNet\\A-TEST\\IBD_CoNet_25";
 
-            LightWeightGraph healthy = LightWeightGraph.GetGraphFromGML($"{healthyPath}");//healthyFile + ".gml");
+            LightWeightGraph healthy = LightWeightGraph.GetGraphFromGML($"{healthyPath}"); //healthyFile + ".gml");
             healthy.SaveGraph(healthyPath + ".graph");
-            LightWeightGraph infected = LightWeightGraph.GetGraphFromGML($"{infectedPath}");//infectedFile + ".gml");
+            LightWeightGraph infected = LightWeightGraph.GetGraphFromGML($"{infectedPath}"); //infectedFile + ".gml");
             infected.SaveGraph(infectedPath + ".graph");
             // Makes a list of what the nodes reference
             using (StreamWriter sw = new StreamWriter(healthyPath + ".txt", true))
@@ -51,6 +60,7 @@ namespace debugNetData
                     sw.WriteLine(healthy.Nodes[i].sharedName);
                 }
             }
+
             using (StreamWriter sw = new StreamWriter(infectedPath + ".txt", true))
             {
                 for (int i = 0; i < infected.Nodes.Length; i++)
@@ -71,49 +81,80 @@ namespace debugNetData
 
             // Now we know the intital number of clusters, do the actual clustering
             //HVATClust clust1 = new HVATClust(lwg2, K, useweights, 1, 0, reassign, hillclimb);
-            HVATClust hclust1 = new HVATClust(healthy, healthyClusters + 1, false, 1, 0, false, false);
-            Partition p1 = hclust1.GetPartition();
-            p1.SavePartition(healthyPath + "_VAT.cluster", healthyPath + ".graph");
 
-            HIntegrityClust hclust2 = new HIntegrityClust(healthy, healthyClusters + 1, false, 1, 0, false, false);
-            Partition p2 = hclust2.GetPartition();
-            p2.SavePartition(healthyPath + "_INT.cluster", healthyPath + ".graph");
+            // This sees if the input cluster type can be parsed as the Enum, and if so 
+            // Uses a switch statement to decide which clustering to run.
+            if (args.Length >= 3)
+            {
+                if (Enum.TryParse<OutType>(args[2], ignoreCase: true, out var userOut))
+                {
+                    switch (userOut)
+                    {
+                        case OutType.INT:
+                            HIntegrityClust hclust2 =
+                                new HIntegrityClust(healthy, healthyClusters + 1, false, 1, 0, false, false);
+                            Partition p2 = hclust2.GetPartition();
+                            p2.SavePartition(healthyPath + "_INT.cluster", healthyPath + ".graph");
 
-            HTenacityClust hclust3 = new HTenacityClust(healthy, healthyClusters + 1, false, 1, 0, false, false);
-            Partition p3 = hclust3.GetPartition();
-            p3.SavePartition(healthyPath + "_TEN.cluster", healthyPath + ".graph");
+                            HIntegrityClust iclust2 =
+                                new HIntegrityClust(infected, infectedClusters + 1, false, 1, 0, false, false);
+                            Partition p5 = iclust2.GetPartition();
+                            p5.SavePartition(infectedPath + "_INT.cluster", infectedPath + ".graph");
 
-            HVATClust iclust1 = new HVATClust(infected, infectedClusters + 1, false, 1, 0, false, false);
-            Partition p4 = iclust1.GetPartition();
-            p4.SavePartition(infectedPath + "_VAT.cluster", infectedPath + ".graph");
+                            int[] clusts2 = new int[p2.DataCount];
+                            int[] clusts5 = new int[p5.DataCount];
+                            // Healthy Group
+                            rename(p2, clusts2, healthyPath, "_INT.csv");
+                            // Infected Group
+                            rename(p5, clusts5, infectedPath, "_INT.csv");
+                            break;
+                        case OutType.TEN:
+                            HTenacityClust hclust3 =
+                                new HTenacityClust(healthy, healthyClusters + 1, false, 1, 0, false, false);
+                            Partition p3 = hclust3.GetPartition();
+                            p3.SavePartition(healthyPath + "_TEN.cluster", healthyPath + ".graph");
 
-            HIntegrityClust iclust2 = new HIntegrityClust(infected, infectedClusters + 1, false, 1, 0, false, false);
-            Partition p5 = iclust2.GetPartition();
-            p5.SavePartition(infectedPath + "_INT.cluster", infectedPath + ".graph");
+                            HTenacityClust iclust3 =
+                                new HTenacityClust(infected, infectedClusters + 1, false, 1, 0, false, false);
+                            Partition p6 = iclust3.GetPartition();
+                            p6.SavePartition(infectedPath + "_TEN.cluster", infectedPath + ".graph");
 
-            HTenacityClust iclust3 = new HTenacityClust(infected, infectedClusters + 1, false, 1, 0, false, false);
-            Partition p6 = iclust3.GetPartition();
-            p6.SavePartition(infectedPath + "_TEN.cluster", infectedPath + ".graph");
+                            int[] clusts3 = new int[p3.DataCount];
+                            int[] clusts6 = new int[p6.DataCount];
+                            // Healthy Group
+                            rename(p3, clusts3, healthyPath, "_TEN.csv");
+                            // Infected Group
+                            rename(p6, clusts6, infectedPath, "_TEN.csv");
+                            break;
+                        case OutType.VAT:
+                            HVATClust hclust1 = new HVATClust(healthy, healthyClusters + 1, false, 1, 0, false, false);
+                            Partition p1 = hclust1.GetPartition();
+                            p1.SavePartition(healthyPath + "_VAT.cluster", healthyPath + ".graph");
 
-            // THE CLUSTERING IS DONE... PUT THE CLUSTERS INTO A MORE USEFUL FORMAT
+                            HVATClust iclust1 =
+                                new HVATClust(infected, infectedClusters + 1, false, 1, 0, false, false);
+                            Partition p4 = iclust1.GetPartition();
+                            p4.SavePartition(infectedPath + "_VAT.cluster", infectedPath + ".graph");
 
-            int[] clusts1 = new int[p1.DataCount];
-            int[] clusts2 = new int[p2.DataCount];
-            int[] clusts3 = new int[p3.DataCount];
-            int[] clusts4 = new int[p4.DataCount];
-            int[] clusts5 = new int[p5.DataCount];
-            int[] clusts6 = new int[p6.DataCount];
-
-            //healthy files
-            rename(p1, clusts1, healthyPath, "_VAT.csv");
-            rename(p2, clusts2, healthyPath, "_INT.csv");
-            rename(p3, clusts3, healthyPath, "_TEN.csv");
-            //infected files
-            rename(p4, clusts4, infectedPath, "_VAT.csv");
-            rename(p5, clusts5, infectedPath, "_INT.csv");
-            rename(p6, clusts6, infectedPath, "_TEN.csv");
-
-        }// brace closes main()
+                            int[] clusts1 = new int[p1.DataCount];
+                            int[] clusts4 = new int[p4.DataCount];
+                            // Healthy Group
+                            rename(p1, clusts1, healthyPath, "_VAT.csv");
+                            // Infected Group
+                            rename(p4, clusts4, infectedPath, "_VAT.csv");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please input a valid output type (VAT, INT, TEN) as the third parameter.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid cluster type (INT, VAT, TEN).");
+            }
+        } // brace closes main()
 
         public static void rename(Partition p, int[] cluster, String FileName, String FileEnd)
         {
@@ -154,7 +195,7 @@ namespace debugNetData
             {
                 Console.WriteLine("combining iteration = " + numMerges + " out of " + (startPartitions - minK));
                 int[,] connections = new int[partition.Clusters.Count, partition.Clusters.Count];
-                LightWeightGraph g = (LightWeightGraph)partition.Data;
+                LightWeightGraph g = (LightWeightGraph) partition.Data;
 
                 // for quick reference let's make a list of which nodes are in which clusters
                 int[] clustAssignments = new int[g.Nodes.Count()];
@@ -162,11 +203,13 @@ namespace debugNetData
                 {
                     clustAssignments[i] = -1;
                 }
+
                 for (int i = 0; i < partition.Clusters.Count; i++)
                 {
                     for (int j = 0; j < partition.Clusters[i].Points.Count; j++)
                     {
-                        clustAssignments[partition.Clusters[i].Points[j].Id] = partition.Clusters[i].Points[j].ClusterId;
+                        clustAssignments[partition.Clusters[i].Points[j].Id] =
+                            partition.Clusters[i].Points[j].ClusterId;
                     }
                 }
                 // right here we should go through and assign the attack nodes to a cluster
@@ -182,8 +225,8 @@ namespace debugNetData
                         {
                             attackNodeStatus[clusterImIn]++;
                         }
-
                     }
+
                     // now go through attackNodeStatus list and find the largest
                     int maxSize = 0;
                     int maxLoc = 0;
@@ -195,9 +238,8 @@ namespace debugNetData
                             clustAssignments[partition.removedNodes[i]] = j;
                         }
                     }
-
-
                 }
+
                 // now removed nodes have been added to a cluster.  There might be some -1 in clustAssignments, so replace those with 0, because we don't know what to do with them
                 for (int i = 0; i < clustAssignments.Length; i++)
                 {
@@ -233,7 +275,7 @@ namespace debugNetData
                         if (j <= i) continue;
                         int sizeI = partition.Clusters[i].Points.Count;
                         int sizeJ = partition.Clusters[j].Points.Count;
-                        double score = ((double)connections[i, j]) / (sizeI * sizeJ);
+                        double score = ((double) connections[i, j]) / (sizeI * sizeJ);
                         //double score = connections[i, j];
                         //if (sizeI > 40 || sizeJ > 40) score = 0;
                         if (score > largestValue)
@@ -242,6 +284,7 @@ namespace debugNetData
                             largestI = i;
                             largestJ = j;
                         }
+
                         // we want to merge smaller into larger clusters
                         if (sizeI > sizeJ)
                         {
@@ -251,11 +294,13 @@ namespace debugNetData
                         }
                     }
                 }
+
                 // if everything's zero, there is no hope ;-)
                 if (largestValue == 0)
                 {
                     continue;
                 }
+
                 List<int> replacedNodes = new List<int>();
                 // Now we need to find the overlap nodes between largestI and largestJ and add them to one of the clusters
                 for (int i = 0; i < partition.removedNodes.Count; i++)
@@ -270,21 +315,23 @@ namespace debugNetData
                             attackNodeStatus[clusterImIn]++;
                         }
                     }
+
                     // THIS CONDITION COULD BE CHANGED!!
                     double std = 0;
 
                     if (attackNodeStatus[largestI] + attackNodeStatus[largestJ] > 0)
                     {
-                        double firstNormalized = (double)attackNodeStatus[largestI] / (attackNodeStatus[largestI] + attackNodeStatus[largestJ]);
-                        double secondNormalized = (double)attackNodeStatus[largestJ] / (attackNodeStatus[largestI] + attackNodeStatus[largestJ]);
+                        double firstNormalized = (double) attackNodeStatus[largestI] /
+                                                 (attackNodeStatus[largestI] + attackNodeStatus[largestJ]);
+                        double secondNormalized = (double) attackNodeStatus[largestJ] /
+                                                  (attackNodeStatus[largestI] + attackNodeStatus[largestJ]);
                         double mean = (firstNormalized + secondNormalized) / 2;
                         std = Math.Sqrt(Math.Pow(firstNormalized - mean, 2) + Math.Pow(secondNormalized - mean, 2));
                     }
                     else std = 1;
 
-                    if (std < 0.4)                    //if (attackNodeStatus[largestI]> 0 && attackNodeStatus[largestJ]>0)
+                    if (std < 0.4) //if (attackNodeStatus[largestI]> 0 && attackNodeStatus[largestJ]>0)
                     {
-
                         //now add to list of replaced nodes
                         replacedNodes.Add(partition.removedNodes[i]);
                         // it is a member of both partitions so add to one of the clusters
@@ -293,14 +340,7 @@ namespace debugNetData
                         partition.Clusters[largestI].AddPoint(newpoint);
                         //now remove from attack set
                         //partition.removedNodes.RemoveAt(i);
-
                     }
-
-
-
-
-
-
                 }
 
 
@@ -342,6 +382,7 @@ namespace debugNetData
                 {
                     partition.MetaData += replacedNodes[i] + " ";
                 }
+
                 partition.MetaData += "\n";
                 // now remove those nodes from the attack set
                 for (int i = 0; i < replacedNodes.Count; i++)
@@ -353,16 +394,16 @@ namespace debugNetData
                             partition.removedNodes.RemoveAt(j);
                         }
                     }
-
                 }
-
             }
+
             partition.MetaData += "Removed Count:" + partition.removedNodes.Count + "\n";
 
             if (partition.removedNodes.Count > 0)
             {
                 partition.MetaData += partition.removedNodes[0];
             }
+
             for (int i = 1; i < partition.removedNodes.Count; i++)
             {
                 partition.MetaData += "," + partition.removedNodes[i];
@@ -371,8 +412,4 @@ namespace debugNetData
             return partition;
         }
     }
-
 }
-
-
-
